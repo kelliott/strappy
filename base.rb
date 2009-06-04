7df995d93090248af29c4aae10fac968fb62c5e0
Kevin Elliott <kevin@phunc.com>
2009-06-04 02:59:07 -0700

    Changed for my personal use.

--- base.rb
+++ base.rb
@@ -1,5 +1,5 @@
 # use this for local installs
-SOURCE=ENV['SOURCE'] || 'http://github.com/pixels-and-bits/strappy/raw/golden'
+SOURCE=ENV['SOURCE'] || 'http://github.com/kelliott/strappy/raw/golden'
 
 def file_append(file, data)
   File.open(file, 'a') {|f| f.write(data) }
@@ -24,20 +24,28 @@ run 'cp config/database.yml config/database.template.yml'
 git :add => "."
 git :commit => "-a -m 'Initial commit'"
 
-# Haml, doing this before gemtools install since we are using 2.1
-if `gem list haml | grep 2.1.0`.chomp == ''
-  unless File.exist?('tmp/haml')
-    inside('tmp') do
-      run 'rm -rf ./haml' if File.exist?('haml')
-      run 'git clone git://github.com/nex3/haml.git'
-    end
-  end
+# Install Gems
+gem 'config_reader'
+gem 'mislav-will_paginate'
+gem 'capistrano'
+gem 'capistrano-ext'
+gem 'haml'
+gem 'authlogic'
+gem 'norman-friendly_id'
+gem 'cucumber', :lib => false
+gem 'rspec', :lib => false
+gem 'rspec-rails', :lib => false
+gem 'rcov', :lib => false
+gem 'webrat', :lib => false
+gem 'ZenTest', :lib => false
+rake('gems:install', :sudo => true)
+rake('gems:unpack', :sudo => true)
+rake('gems:build', :sudo => true)
+git :add => "."
+git :commit => "-a -m 'Added Gems'"
 
-  inside('tmp/haml') do
-    run 'rake install'
-  end
-end
 
+# Hamlify the project
 run 'echo N\n | haml --rails .'
 run 'mkdir -p public/stylesheets/sass'
 %w(
@@ -57,32 +65,16 @@ end
 git :add => "."
 git :commit => "-a -m 'Added Haml and Sass stylesheets'"
 
-# GemTools
-file 'config/gems.yml', open("#{SOURCE}/config/gems.yml").read
-run 'sudo gem install gem_tools --no-rdoc --no-ri'
-run 'sudo gemtools install'
-initializer 'gem_tools.rb', "require 'gem_tools'\nGemTools.load_gems"
-git :add => "."
-git :commit => "-a -m 'Added GemTools config'"
-
-# CoreExtensions
-plugin 'core_extensions',
-  :git => 'git://github.com/UnderpantsGnome/core_extensions.git'
-git :add => "."
-git :commit => "-a -m 'Added Core Extensions'"
-
-# install strappy rake tasks
-rakefile 'strappy.rake', open("#{SOURCE}/lib/tasks/strappy.rake").read
-
 # install haml rake tasks
 rakefile 'haml.rake', open("#{SOURCE}/lib/tasks/haml.rake").read
 
-# RSpec
+# RSpec / Cucumber
 generate 'rspec'
+generate 'cucumber'
 file 'spec/rcov.opts', open("#{SOURCE}/spec/rcov.opts").read
 file_append('spec/spec_helper.rb', open("#{SOURCE}/spec/helpers.rb").read)
 git :add => "."
-git :commit => "-a -m 'Added RSpec'"
+git :commit => "-a -m 'Generated RSpec and Cucumber'"
 
 # SiteConfig
 file 'config/site.yml', open("#{SOURCE}/config/site.yml").read
@@ -99,59 +91,17 @@ git :commit => "-a -m 'Added cruise rake task'"
 capify!
 file 'config/deploy.rb', open("#{SOURCE}/config/deploy.rb").read
 
-%w( production staging ).each do |env|
+%w( alpha production staging ).each do |env|
   file "config/deploy/#{env}.rb", "set :rails_env, \"#{env}\""
 end
 
 inside('config/environments') do
+  run 'cp development.rb alpha.rb'
   run 'cp development.rb staging.rb'
 end
 
 git :add => "."
 git :commit => "-a -m 'Added Capistrano config'"
-
-# jRails
-plugin 'jrails', :svn => 'http://ennerchi.googlecode.com/svn/trunk/plugins/jrails'
-
-# remove the installed files, we're using a newer version below
-inside('public/javascripts') do
-  %w(
-    jquery-ui.js
-    jquery.js
-  ).each do |file|
-    run "rm -f #{file}"
-  end
-end
-
-git :add => "."
-git :commit => "-a -m 'Added jRails plugin'"
-
-# jQuery
-
-# clean up prototype files
-inside('public/javascripts') do
-  %w(
-    application.js
-    controls.js
-    dragdrop.js
-    effects.js
-    prototype.js
-  ).each do |file|
-    run "rm -f #{file}"
-  end
-end
-
-file 'public/javascripts/jquery.js',
-  open('http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js').read
-file 'public/javascripts/jquery-ui.js',
-  open('http://ajax.googleapis.com/ajax/libs/jqueryui/1.7/jquery-ui.min.js').read
-file 'public/javascripts/jquery.form.js',
-  open('http://github.com/malsup/form/raw/master/jquery.form.js').read
-file 'public/javascripts/application.js',
-  open("#{SOURCE}/public/javascripts/application.js").read
-
-git :add => "."
-git :commit => "-a -m 'Added jQuery with UI and form plugin'"
 
 # Blackbird
 run 'mkdir -p public/blackbird'
